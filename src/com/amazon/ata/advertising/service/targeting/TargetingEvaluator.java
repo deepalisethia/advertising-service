8c0824c018e66a87982f3661e34158b362025831
Deepali Sethia <deepali.sethia@gmail.com>
2022-09-15 16:12:15 -0700

    Mastery Task 2

--- src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
+++ src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
@@ -4,9 +4,12 @@ import com.amazon.ata.advertising.service.model.RequestContext;
 import com.amazon.ata.advertising.service.targeting.predicate.TargetingPredicate;
 import com.amazon.ata.advertising.service.targeting.predicate.TargetingPredicateResult;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
@@ -14,7 +17,7 @@ import java.util.stream.Collectors;
  */
 public class TargetingEvaluator {
     public static final boolean IMPLEMENTED_STREAMS = true;
-    public static final boolean IMPLEMENTED_CONCURRENCY = false;
+    public static final boolean IMPLEMENTED_CONCURRENCY = true;
     private final RequestContext requestContext;
 
     /**
@@ -32,9 +35,38 @@ public class TargetingEvaluator {
      * @return TRUE if all of the TargetingPredicates evaluate to TRUE against the RequestContext, FALSE otherwise.
      */
     public TargetingPredicateResult evaluate(TargetingGroup targetingGroup) {
-//        ORIGINAL CODE
+
 //        boolean allTruePredicates = true;
 //        List<TargetingPredicate> targetingPredicates = targetingGroup.getTargetingPredicates();
+
+        // Create an Executor Service
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        // Create a Futures List
+//        List<Future<TargetingPredicate>> futureList = new ArrayList<>();
+//
+//        for (TargetingPredicate predicate : targetingPredicates) {
+//            // What is the task to submit?? Which class implements Callable or do we have to change one?
+//            TargetingPredicateResult predicateResult = predicate.evaluate(requestContext);
+//            if (predicateResult.isTrue()) {
+//                futureList.add()
+//            }
+//        }
+
+
+        // TRIAL TO GET TIMEOUT EXCEPTION TO WORK
+//        List<Future<Boolean>> truePredicateResults = new ArrayList<>();
+//        for (Future<Boolean> result : truePredicateResults) {
+//            try {
+//                if (!result.get()) {
+//                    allTruePredicates = false;
+//                }
+//            } catch (TimeoutException e) {
+//                allTruePredicates = false;
+//            }
+//        }
+
+
 //        for (TargetingPredicate predicate : targetingPredicates) {
 //            TargetingPredicateResult predicateResult = predicate.evaluate(requestContext);
 //            if (!predicateResult.isTrue()) {
@@ -42,18 +74,40 @@ public class TargetingEvaluator {
 //                break;
 //            }
 //        }
+
+//        for (Future<TargetingPredicate> targetingPredicateFuture : futureList) {
+//            try {
+//                targetingPredicates.add(targetingPredicateFuture.get());
+//            } catch (ExecutionException | InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            // catch (TimeoutException e) {
+//                System.out.println();
+//                allTruePredicates = false;
+
+
 //        return allTruePredicates ? TargetingPredicateResult.TRUE :
 //                                   TargetingPredicateResult.FALSE;
 
 
-
-        // Updating Logic to Use A Stream
-        return targetingGroup
-                .getTargetingPredicates()
-                .stream()
-                .allMatch(predicate -> predicate.evaluate(requestContext).isTrue())
-                ? TargetingPredicateResult.TRUE :
-                TargetingPredicateResult.FALSE;
+        try {
+            return executorService.submit(() -> targetingGroup
+                    .getTargetingPredicates()
+                    .parallelStream()
+                    .allMatch(predicate -> predicate.evaluate(requestContext).isTrue())
+                    ? TargetingPredicateResult.TRUE :
+                    TargetingPredicateResult.FALSE).get();
+            } catch (ExecutionException | InterruptedException | CancellationException e) {
+                        e.printStackTrace();
+            }
+        executorService.shutdown();
+        return TargetingPredicateResult.FALSE;
+//        return targetingGroup
+//                .getTargetingPredicates()
+//                .stream()
+//                .allMatch(predicate -> predicate.evaluate(requestContext).isTrue())
+//                ? TargetingPredicateResult.TRUE :
+//                TargetingPredicateResult.FALSE;
 
 
     }
